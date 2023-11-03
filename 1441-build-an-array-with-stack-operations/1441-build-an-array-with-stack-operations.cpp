b4bb6e2cbdbe29175ccabb88059bc476eed761c4
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2023-11-03 15:46:52 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (49.67%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -2,16 +2,27 @@ class Solution {
 public:
     vector<string> buildArray(vector<int>& t, int n) {
         vector<string> ans;
-        unordered_map<int, bool> umpp;
-        int mx = t[t.size()-1];
-        for(auto i:t) umpp[i] = true;
-        for(int i = 1; i <= n; i++){
-            if(i == mx+1) break;
-            if(umpp[i]){
-                ans.push_back("Push");
-            }else{
+        // unordered_map<int, bool> umpp;
+        // int mx = t[t.size()-1];
+        // for(auto i:t) umpp[i] = true;
+        // for(int i = 1; i <= n; i++){
+        //     if(i == mx+1) break;
+        //     if(umpp[i]){
+        //         ans.push_back("Push");
+        //     }else{
+        //         ans.push_back("Push");
+        //         ans.push_back("Pop");
+        //     }
+        // }
+        // return ans;
+        int idx = 0;
+        for(int i = 1; i <= n && idx < t.size(); i++){
+            if(i < t[idx]){
                 ans.push_back("Push");
                 ans.push_back("Pop");
+            }else{
+                ans.push_back("Push");
+                idx++;
             }
         }
         return ans;
