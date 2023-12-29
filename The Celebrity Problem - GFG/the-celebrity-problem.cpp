16ba23225cadef594bc7a83d64a8073be2e343e4
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2023-12-29 12:18:01 +0530

    Added solution - LeetHub

--- The Celebrity Problem - GFG/the-celebrity-problem.cpp
+++ The Celebrity Problem - GFG/the-celebrity-problem.cpp
@@ -16,38 +16,55 @@ class Solution
     //Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int>>& M, int n) 
     {
-        if(n == 1 && M[0][0] == 0) return 0;
-	    bool flag = false;
-	    for(int i = 0; i < n; i++) {
-	        for(int j = 0; j < n; j++){
-	            if(M[i][j]) {
-	                flag = true;
-	                break;
-	            }
-	        }
-	    }
-	    int cele = -1;
-	    if(flag){
-	        for(int i = 0; i < n; i++){
-	            bool isCele = true;
-	            for(int j = 0; j < n; j++){
-	                if(i!=j && knows(M, i, j)) {
-	                    isCele = false;
-	                    break;
-	                }
-	            }
-	            if(isCele) {
-	                cele = i;
-	                break;
-	            }
-	        }
-	        if(cele != -1){
-	            for(int i = 0; i < n; i++){
-	                if(i != cele && M[i][cele] == false) return -1;
-	            }
-	        }
-	    }
-	    return cele;
+    //     if(n == 1 && M[0][0] == 0) return 0;
+	   // bool flag = false;
+	   // for(int i = 0; i < n; i++) {
+	   //     for(int j = 0; j < n; j++){
+	   //         if(M[i][j]) {
+	   //             flag = true;
+	   //             break;
+	   //         }
+	   //     }
+	   // }
+	   // int cele = -1;
+	   // if(flag){
+	   //     for(int i = 0; i < n; i++){
+	   //         bool isCele = true;
+	   //         for(int j = 0; j < n; j++){
+	   //             if(i!=j && knows(M, i, j)) {
+	   //                 isCele = false;
+	   //                 break;
+	   //             }
+	   //         }
+	   //         if(isCele) {
+	   //             cele = i;
+	   //             break;
+	   //         }
+	   //     }
+	   //     if(cele != -1){
+	   //         for(int i = 0; i < n; i++){
+	   //             if(i != cele && M[i][cele] == false) return -1;
+	   //         }
+	   //     }
+	   // }
+	   // return cele;
+	   
+	   int A = 0, B = 1;
+
+        // Find a potential celebrity using two-pointer approach.
+        while (B < n) {
+            if (M[A][B])
+                A = B;
+            B++;
+        }
+    
+        // Verify the potential celebrity.
+        for (int i = 0; i < n; i++) {
+            if (i != A && (M[A][i] || !M[i][A]))
+                return -1;
+        }
+    
+        return A;
     }
 };
 
