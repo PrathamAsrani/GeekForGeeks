4f7e093a5e41c669e394e616b8282a1d4a8e50d3
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2023-12-30 15:10:44 +0530

    Time: 342 ms (92.17%), Space: 165.6 MB (72.54%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,42 +1,76 @@
 class LRUCache {
 public:
-    unordered_map<int, int> umpp;
-    unordered_map<int, list<int>::iterator> addr;
-    list<int> l;
-    int cap, size;
-    LRUCache(int cap) {
-        size = 0;
-        this->cap = cap;
+//     unordered_map<int, int> umpp;
+//     unordered_map<int, list<int>::iterator> addr;
+//     list<int> l;
+//     int cap, size;
+//     LRUCache(int cap) {
+//         size = 0;
+//         this->cap = cap;
+//     }
+    
+//     int get(int key) {
+//         if(umpp.find(key) == umpp.end()) return -1;
+//         list<int>::iterator it = addr[key];
+//         l.erase(it);
+//         addr.erase(key);
+//         l.push_front(key);
+//         addr[key]=l.begin();
+//         return umpp[key];
+//     }
+    
+//     void put(int key, int value) {
+//         if(umpp.find(key) != umpp.end()){
+//             l.erase(addr[key]);
+//             addr.erase(key);
+//             umpp.erase(key);
+//             size--;
+//         }
+//         if(size >= cap){
+//             int delKey = l.back();
+//             l.pop_back();
+//             addr.erase(delKey);
+//             umpp.erase(delKey);
+//             size--;
+//         }
+//         size++;
+//         l.push_front(key);
+//         addr[key] = l.begin();
+//         umpp[key] = value;
+//     }
+    
+    public:
+    list<pair<int,int>> l;
+    unordered_map<int, list<pair<int,int>>::iterator> mp;
+    int size;
+    LRUCache(int capacity) {
+        size = capacity;
     }
     
     int get(int key) {
-        if(umpp.find(key) == umpp.end()) return -1;
-        list<int>::iterator it = addr[key];
-        l.erase(it);
-        addr.erase(key);
-        l.push_front(key);
-        addr[key]=l.begin();
-        return umpp[key];
+        if(mp.find(key)!=mp.end())
+        {
+            l.splice(l.begin(), l, mp[key]);
+            return mp[key]->second;
+        }
+        return -1;
     }
     
     void put(int key, int value) {
-        if(umpp.find(key) != umpp.end()){
-            l.erase(addr[key]);
-            addr.erase(key);
-            umpp.erase(key);
-            size--;
+        if(mp.find(key)!=mp.end())
+        {
+            l.splice(l.begin(), l, mp[key]);
+            mp[key]->second = value;
+            return;
         }
-        if(size >= cap){
-            int delKey = l.back();
+        if(l.size()==size)
+        {
+            int dkey = l.back().first;
             l.pop_back();
-            addr.erase(delKey);
-            umpp.erase(delKey);
-            size--;
+            mp.erase(dkey);
         }
-        size++;
-        l.push_front(key);
-        addr[key] = l.begin();
-        umpp[key] = value;
+        l.push_front(make_pair(key, value));
+        mp[key] = l.begin();
     }
 };
 
