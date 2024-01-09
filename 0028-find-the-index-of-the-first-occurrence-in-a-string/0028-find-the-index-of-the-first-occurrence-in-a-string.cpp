a43bc4b531b87e85255430ca1d3befeac5411e39
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-01-09 13:35:14 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (28.98%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -13,7 +13,8 @@ public:
         }
         return pi;
     }
-    int strStr(string t, string s) {
+    int strStr(string /*t*/ txt, string /*s*/ pat) {
+        /*
         vector<int> prefix = prefix_function(s);
         int j = 0, i = 0;
         while(i < t.size()){
@@ -29,6 +30,21 @@ public:
             }
             if(j == s.size())
                 return i-s.size();
+        }
+        return -1;
+        */
+        int m = pat.size(), n = txt.size();
+        if(m > n)
+            return -1;
+        char start = pat[0];
+        for(int i = 0; i < n; i++){
+            if(txt[i] == start){
+                string tmp;
+                if(i+m <= n) 
+                    tmp = txt.substr(i, m);
+                if(tmp == pat) 
+                    return i;
+            }
         }
         return -1;
     }
