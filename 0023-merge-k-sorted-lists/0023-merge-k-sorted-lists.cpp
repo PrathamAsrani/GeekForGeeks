fef78f5704404909d4096649e5197cc904de4259
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-01-16 23:48:13 +0530

    Time: 14 ms (79.81%), Space: 13.6 MB (65.12%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,6 +8,14 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+class compare{
+public:
+    bool operator()(ListNode* a, ListNode *b){
+        return a->val > b->val;
+    }
+};
+
 class Solution {
 public:
     ListNode *merge(ListNode *list1, ListNode *list2) {
@@ -50,6 +58,7 @@ public:
       return start;
     }
     ListNode* mergeKLists(vector<ListNode*>& listArray) {
+        /*
         int n = listArray.size();
         if(n == 0)
             return nullptr;
@@ -61,5 +70,29 @@ public:
             start = merge(a, start);
         }
         return start;
+        */
+        
+        int n = listArray.size();
+        if(n == 0)
+            return nullptr;
+        if(n==1)
+            return listArray[0];
+        priority_queue<ListNode *, vector<ListNode *>, compare> pq;
+        for(int i = 0 ; i < n; i++)
+            if(listArray[i] != nullptr) pq.push(listArray[i]);
+        
+        ListNode *ans = new ListNode(-1);
+        ListNode *tmp = ans;
+    
+        while(!pq.empty()){
+            ListNode *top = pq.top();
+            pq.pop();
+            if(top->next)
+                pq.push(top->next);
+            
+            tmp->next = top;
+            tmp = top;
+        }
+        return ans->next;
     }
 };
